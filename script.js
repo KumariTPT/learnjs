cde766b61ec0d014b59c716a81f51e625f82a060
Kristofer Kumari <kristofer.kumari@tptlive.ee>
2023-09-25 10:52:52 +0300

    WASD mäng

--- script.js
+++ script.js
@@ -5,56 +5,25 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
-ctx.rect(10, 10, 50, 30);
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 1;
-ctx.stroke();
+let x = 10;
+let y = 10;
+setInterval(() => {
+    ctx.reset();
+    ctx.fillRect(x,y, 30, 30);
+}, 16);
 
-ctx.fillStyle = 'black';
-ctx.fillRect(10, 10, 50, 20);
-
-ctx.fillStyle = 'blue';
-ctx.fillRect(10, 10, 50, 10);
-
-
-
-ctx.moveTo(100,50);
-ctx.lineTo(130, 80);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.moveTo(100,80);
-ctx.lineTo(150,50);
-ctx.stroke();
-ctx.beginPath();
-
-
-ctx.moveTo(100, 50);
-ctx.lineTo(150,50);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.moveTo(100,80);
-ctx.lineTo(125, 40);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.moveTo(125,40);
-ctx.lineTo(130, 80);
-ctx.stroke();
-ctx.beginPath();
-
-
-ctx.arc(200, 100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'green';
-ctx.lineWidth = 1;
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineTo(200, 100);
-ctx.arc(200, 100, 50, -2, -3 / Math.PI);
-ctx.fillStyle = 'green'
-ctx.fill();
-ctx.strokeStyle = 'green';
-ctx.lineWidth = 1;
-ctx.stroke();
+canvas.addEventListener('keydown', event => {
+    console.log(event)
+    if(event.code == 'KeyW'){
+        y-=5;
+    }
+    if(event.code == 'KeyS'){
+        y+=5;
+    }
+    if(event.code == 'KeyD'){
+        x+=5;
+    }
+    if(event.code == 'KeyA'){
+        x-=5;
+    }
+});
