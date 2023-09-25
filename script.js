868e32167ba8a054bcba0b454a4e32b003087c69
Kristofer Kumari <kristofer.kumari@tptlive.ee>
2023-09-25 10:27:42 +0300

    hindeline töö

--- script.js
+++ script.js
@@ -4,16 +4,57 @@
 //}, 5000);
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+
+ctx.rect(10, 10, 50, 30);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.fillRect(10, 10, 50, 20);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(10, 10, 50, 10);
+
+
+
+ctx.moveTo(100,50);
+ctx.lineTo(130, 80);
 ctx.stroke();
 ctx.beginPath();
-ctx.arc(100,100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'red';
-ctx.lineWidth = 3;
+
+ctx.moveTo(100,80);
+ctx.lineTo(150,50);
 ctx.stroke();
-ctx.font = "30px Arial";
-ctx.fillText('Hello Canvas', 30, 50);
-ctx.strokeText('Hello Canvas', 30, 100);
-ctx.fillStyle = 'red';
-ctx.fillRect(10, 10, 10, 10);
+ctx.beginPath();
+
+
+ctx.moveTo(100, 50);
+ctx.lineTo(150,50);
+ctx.stroke();
+ctx.beginPath();
+
+ctx.moveTo(100,80);
+ctx.lineTo(125, 40);
+ctx.stroke();
+ctx.beginPath();
+
+ctx.moveTo(125,40);
+ctx.lineTo(130, 80);
+ctx.stroke();
+ctx.beginPath();
+
+
+ctx.arc(200, 100, 50, 0, 2 * Math.PI);
+ctx.strokeStyle = 'green';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.lineTo(200, 100);
+ctx.arc(200, 100, 50, -2, -3 / Math.PI);
+ctx.fillStyle = 'green'
+ctx.fill();
+ctx.strokeStyle = 'green';
+ctx.lineWidth = 1;
+ctx.stroke();
